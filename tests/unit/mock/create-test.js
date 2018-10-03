d5e3b24a6218259a1268b4071333cf4380585eaf
Rocky Neurock <jneurock@gmail.com>
2018-10-03 23:07:50 +0200

    add rest for mock create

---
+++ tests/unit/mock/create-test.js
@@ -0,0 +1,17 @@
+import { createMocksForSchema } from 'ember-cli-mirage-graphql/mock/create';
+import { module, test } from 'qunit';
+
+module('Unit | Mock | create', function() {
+  test('it creates mocking functions for root schema types', function(assert) {
+    let schema = {
+      _mutationType: { name: 'Mutation', _fields: { foo: 'bar' } },
+      _queryType: { name: 'Query', _fields: { foo: 'bar' } }
+    };
+    let mocks = createMocksForSchema(schema);
+    let mutationMock = mocks.Mutation();
+    let queryMock = mocks.Query();
+
+    assert.equal(typeof mutationMock.foo, 'function', 'Mutation foo is mocked');
+    assert.equal(typeof queryMock.foo, 'function', 'Query foo is mocked');
+  });
+});
