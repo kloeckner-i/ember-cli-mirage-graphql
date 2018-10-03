01fd92ddaa7989e566f8ddd53611eeec4e20d24b
Rocky Neurock <jneurock@gmail.com>
2018-10-03 23:06:16 +0200

    remove some dead code

--- addon/utils.js
+++ addon/utils.js
@@ -1,31 +1,14 @@
-import MockInfo from './mock/info';
-import { get } from '@ember/object';
+const pipeReducer = (f, g) => (...args) => g(f(...args));
 
 export const isFunction = (obj) => obj != null && typeof obj === 'function';
 
-export const maybeUnwrapInterfaceType = (fieldNodes = [], { _typeMap }) =>
-  (mockInfo) => {
-    let [node] = fieldNodes;
-
-    if (node) {
-      let { selections = [] } = node.selectionSet;
-      let selection = selections.find((s) => s.typeCondition) || {};
-      let typeName = get(selection, 'typeCondition.name.value');
-
-      if (typeName) {
-        return MockInfo.create({ returnType: _typeMap[typeName] });
-      }
-    }
-
-    return mockInfo;
-  };
-
 export const pipe = (...fns) => fns.reduce(pipeReducer);
+
+export const reduceKeys = (obj, reducerFn, defaultValue) =>
+  Object.keys(obj).reduce(reducerFn, defaultValue);
 
 export function contextSet(obj, k, v) {
   obj[k] = v;
 
   return obj;
 }
-
-const pipeReducer = (f, g) => (...args) => g(f(...args));
