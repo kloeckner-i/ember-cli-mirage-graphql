fbfdc7573baf9c503bf205eecb4be6d3d769093c
Rocky Neurock <jneurock@gmail.com>
2019-01-24 21:22:00 +0100

    added test for field names

---
+++ tests/unit/fields/name-test.js
@@ -0,0 +1,46 @@
+import {
+  getFieldName,
+  resolveFieldName
+} from 'ember-cli-mirage-graphql/fields/name';
+import { module, test } from 'qunit';
+
+module('Unit | Fields | name', function() {
+  test('it can get a field name or alias', function(assert) {
+    let fieldName = 'foo';
+    let field = {
+      name: { value: fieldName }
+    };
+    let fieldAlias = 'baz';
+    let fieldWithAlias = {
+      alias: { value: fieldAlias },
+      name: { value: 'bar' }
+    };
+
+    assert.equal(getFieldName(field), fieldName, 'It can get a name');
+    assert.equal(getFieldName(fieldWithAlias), fieldAlias, 'It can get an alias');
+  });
+
+  test('it can resolve a non-mapped field name', function(assert) {
+    let fieldName = 'foo';
+
+    assert.equal(resolveFieldName(fieldName), fieldName,
+      'It resolved the non-mapped name');
+  });
+
+  test('it can resolve a mapped field name', function(assert) {
+    let fieldName = 'bar';
+    let parentTypeName = 'Foo';
+    let resolvedFieldName = 'baz';
+    let options = {
+      fieldsMap: {
+        [parentTypeName]: {
+          [fieldName]: resolvedFieldName
+        }
+      }
+    };
+    let parent = { field: { type: { name: parentTypeName } } };
+
+    assert.equal(resolveFieldName(fieldName, parent, options),
+      resolvedFieldName, 'It resolved the mapped name');
+  });
+});
