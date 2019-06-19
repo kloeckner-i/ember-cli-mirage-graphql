f702ee6492c1eb1f25f72b71c3d8bb966cf1942a
Jamie White <jamie@jgwhite.co.uk>
2019-06-19 08:49:00 +0100

    Unwrap non-null types (#20)
    * Unwrap non-null types during selection type inference
    
    We ran into the following error when we had a non-null `edges` field in
    our schema (with a non-null type within the collection):
    
        Cannot read property 'cursor' of undefined
    
    This patch adds a similar non-null `edges` field to the schema and adds
    code to unwrap non-null types when inferring the type of a selection.
    
    * Unwrap non-null returnType during mutations

--- addon/fields/selections/index.js
+++ addon/fields/selections/index.js
@@ -1,10 +1,20 @@
 import createFieldInfo from '../info/create';
 import { getFieldNameAndAlias } from '../name';
 import { fragmentFieldsReducer, inlineFragmentFieldsReducer } from './fragments';
-import { partial } from '../../utils';
+import { partial, unwrapNonNull } from '../../utils';
 
-const getFieldType = (type, fieldName) =>
-  fieldName !== '__typename' && type._fields[fieldName].type;
+const getFieldType = (type, fieldName) => {
+  if (fieldName === '__typename') {
+    return null;
+  }
+
+  type = unwrapNonNull(type);
+
+  let field = type._fields[fieldName];
+  let fieldType = unwrapNonNull(field.type);
+
+  return fieldType;
+}
 
 export const selectedFieldsReducer =
   (getFieldNameAndAlias, getFieldType, createFieldInfo, getType, type, fragments, selections, selection) => {
