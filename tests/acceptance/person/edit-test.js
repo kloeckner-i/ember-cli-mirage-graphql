5bb2e7ef4cb5bcfedb10af26da772364fca2b042
David Mazza <davidedwardmazza@gmail.com>
2019-11-30 13:03:11 -0800

    create multiple people in the test and visit person/:id to verify change the edit-by-name test in the same way

--- tests/acceptance/person/edit-test.js
+++ tests/acceptance/person/edit-test.js
@@ -24,39 +24,47 @@ module('Acceptance | edit person', function(hooks) {
   });
 
   test('it can update a person by name', async function(assert) {
-    let person = server.create('person', { surname: 'Smith' });
+    let people = [
+      server.create('person', { surname: 'Smith' }),
+      server.create('person', { surname: 'Wayne' }),
+      server.create('person', { surname: 'Forst' })
+    ];
     let newLastName = 'Jones';
 
-    await visit(`/person/${person.id}/edit-by-name`);
+    await visit(`/person/${people[1].id}/edit-by-name`);
 
     let lastNameInput = this.element.querySelector('.person-last-name');
 
-    assert.equal(lastNameInput.value, person.surname, 'It has last name');
+    assert.equal(lastNameInput.value, people[1].surname, 'It has last name');
 
     await fillIn(lastNameInput, newLastName);
     await click('.person-save');
-    await visit('/people');
+    await visit(`/person/${people[1].id}`);
 
-    let lastName = this.element.querySelector('.people-last-name');
+    let lastName = this.element.querySelector('.person-last-name');
 
     assert.equal(lastName.textContent, newLastName, 'The last name changed');
   });
 
   test('it can update a person like Prisma would', async function(assert) {
-    let person = server.create('person', { surname: 'Smith' });
+    let people = [
+      server.create('person', { surname: 'Smith' }),
+      server.create('person', { surname: 'Wayne' }),
+      server.create('person', { surname: 'Forst' })
+    ];
     let newLastName = 'Jones';
 
-    await visit(`/person/${person.id}/edit-like-prisma`);
+    await visit(`/person/${people[1].id}/edit-like-prisma`);
 
     let lastNameInput = this.element.querySelector('.person-last-name');
 
-    assert.equal(lastNameInput.value, person.surname, 'It has last name');
+    assert.equal(lastNameInput.value, people[1].surname, 'It has last name');
 
     await fillIn(lastNameInput, newLastName);
     await click('.person-save');
-    await visit('/people');
+    await visit(`/person/${people[1].id}`);
 
-    let lastName = this.element.querySelector('.people-last-name');
+    let lastName = this.element.querySelector('.person-last-name');
 
     assert.equal(lastName.textContent, newLastName, 'The last name changed');
   });
